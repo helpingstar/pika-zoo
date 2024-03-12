6ce45b30f1afb153e3fde0cc756a71436f718886
helpingstar <iamhelpingstar@gmail.com>
2024-03-12 11:36:10 +0900

    Fix error

--- pikazoo/wrappers/record_episode_statistics.py
+++ pikazoo/wrappers/record_episode_statistics.py
@@ -12,14 +12,14 @@ class RecordEpisodeStatistics(BaseParallelWrapper):
     def __init__(self, env: pettingzoo.ParallelEnv):
         BaseParallelWrapper.__init__(self, env)
         self.agents = self.env.agents
-        self.episode_rewards = {agent: 0 for agent in self.agents}
-        self.episode_lengths = {agent: 0 for agent in self.agents}
+        self.episode_rewards = {agent: 0 for agent in self.possible_agents}
+        self.episode_lengths = {agent: 0 for agent in self.possible_agents}
 
     def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
         """Resets the environment, recording episode statistics."""
         obs, info = super().reset(seed, options)
         self.agents = self.env.agents
-        for agent in self.agents:
+        for agent in self.possible_agents:
             self.episode_rewards[agent] = 0
             self.episode_lengths[agent] = 0
         return obs, info
@@ -28,11 +28,11 @@ class RecordEpisodeStatistics(BaseParallelWrapper):
         """Steps through the environment, recording episode statistics."""
         obs, rews, terminateds, truncateds, infos = super().step(action)
         self.agents = self.env.agents
-        for agent in self.agents:
+        for agent in self.possible_agents:
             self.episode_rewards[agent] += rews[agent]
             self.episode_lengths[agent] += 1
         if all(terminateds.values()) or all(truncateds.values()):
-            for agent in self.agents:
+            for agent in self.possible_agents:
                 infos[agent]["episode"] = {
                     "r": self.episode_rewards[agent],
                     "l": self.episode_lengths[agent],
